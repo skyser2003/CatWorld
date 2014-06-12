fe7bd21257fa5b92a9f0732096d95c5624b8a55c
Seung Ho Jang <shjang1992@hotmail.com>
2014-06-13 02:09:00 +0900

    Postbuild script modified - copies the result module into nodejs server folder

--- Server/cpp_source/Server/postbuild.js
+++ Server/cpp_source/Server/postbuild.js
@@ -4,11 +4,12 @@ var exec = require('child_process').exec;
 // Generate binding.gyp
 var cppRegex = new RegExp(/^.*\.cpp$/);
 var files = fs.readdirSync(__dirname);
+var targetName = "server";
 
 var binding = {};
 binding.targets = [];
 binding.targets[0] = {};
-binding.targets[0].target_name = "server";
+binding.targets[0].target_name = targetName;
 binding.targets[0].sources = [];
 
 for (var i in files)
@@ -24,5 +25,8 @@ fs.writeFile("binding.gyp", JSON.stringify(binding));
 
 // Activate node-gyp
 exec("node-gyp configure build");
+
+// Copy generated file to express server's node_modules
+fs.createReadStream("build/Release/" + targetName + ".node").pipe(fs.createWriteStream("../../nodejs_source/node_modules/" + targetName + ".node"));
 
 console.log("Postbuild success");
