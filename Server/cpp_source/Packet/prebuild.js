2f33d8e32a4fcebc6acfd4cb9d6f64fb067b3bc0
Seung Ho Jang <shjang1992@hotmail.com>
2014-06-14 02:37:21 +0900

    Protobuf build setting added

--- Server/cpp_source/Packet/prebuild.js
+++ Server/cpp_source/Packet/prebuild.js
@@ -1,5 +1,27 @@
 var fs = require('fs');
 var exec = require('child_process').exec;
 
-var protoc = __dirname + "/../External/protoc.exe";
-console.log(protoc);
+// Generate proto files
+var protoc = __dirname + "\\..\\External\\protoc.exe";
+var protoRegex = new RegExp(/^.*\.proto$/);
+var files = fs.readdirSync(__dirname);
+
+var protoFiles = [];
+
+for (var i in files) {
+    var file = files[i];
+    if (protoRegex.test(file) === true) {
+        protoFiles.push(file);
+    }
+}
+
+var execString = protoc +
+    " --cpp_out=\"" + __dirname + "\\Autogen\"" +
+    " -I" + __dirname;
+for (var i in protoFiles) {
+    execString += " " + __dirname + "\\" + protoFiles[i];
+}
+
+console.log(execString);
+
+exec(execString);
