485ae614ee5036b32cb39a9b0e0d7ae119f7a179
Seung Ho Jang <shjang1992@hotmail.com>
2014-06-15 19:05:50 +0900

    Removed unusing code

--- cpp_source/Server/main.cpp
+++ cpp_source/Server/main.cpp
@@ -3,11 +3,6 @@
 #include "ServerModule.h"
 using namespace v8;
 
-Handle<Value> Method(const Arguments& args) {
-	HandleScope scope;
-	return scope.Close(String::New("world"));
-}
-
 void init(Handle<Object> exports) {
 	ServerModule::Export(exports);
 }
