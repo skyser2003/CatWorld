73bc93b15bd2b6db68451c6759b6c026cb47dda0
Seung Ho Jang <shjang1992@hotmail.com>
2014-06-23 02:33:09 +0900

    Simple initialize function added

--- cpp_source/ServerLib/Client.cpp
+++ cpp_source/ServerLib/Client.cpp
@@ -1,3 +1,8 @@
 #include "stdafx.h"
-
 #include "Client.h"
+
+void Client::Init(std::string id, std::string pw)
+{
+	this->id = id;
+	this->pw = pw;
+}
--- cpp_source/ServerLib/Client.h
+++ cpp_source/ServerLib/Client.h
@@ -3,5 +3,8 @@
 class Client
 {
 public:
+	void Init(std::string id, std::string pw);
 private:
+	std::string id;
+	std::string pw;
 };
