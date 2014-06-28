40a28d99173eb6f8f195ec8113d6230932e217e8
Seung Ho Jang <shjang1992@hotmail.com>
2014-06-29 00:40:29 +0900

    FieldObject, FieldObjectFSM added

--- nodejs_source/public/js/Logic/Field/fieldobject.js
+++ nodejs_source/public/js/Logic/Field/fieldobject.js
@@ -1,3 +1,7 @@
 var FieldObject = function () {
-    this.pos = new THREE.Vector3(0, 0, 0);
+    this.pos = new Vector3(0, 0, 0);
+    this.dest = new Vector3(0, 0, 0);
+
+    this.fsmEnum = FieldObjectFSM.STATE_IDLE;
+    this.fsm = new FieldObjectFSM_IDLE(this);
 };
---
+++ nodejs_source/public/js/Logic/Field/fieldobjectfsm.js
@@ -0,0 +1,35 @@
+var FieldObjectFSM = {
+    STATE_IDLE: 0,
+    STATE_MOVE: 1
+};
+
+var FieldObjectFSM_IDLE = function (fieldObject) {
+    this.fieldObject = fieldObject;
+
+    this.onBegin() = function () {
+    };
+
+    this.onUpdate() = function () {
+
+    };
+
+    this.onEnd() = function () {
+
+    };
+};
+
+var FieldObjectFSM_MOVE = function (fieldObject) {
+    this.fieldObject = fieldObject;
+
+    this.onBegin = function () {
+
+    };
+
+    this.onUpate() = function () {
+
+    };
+
+    this.onEnd() = function () {
+
+    };
+};
