f4ff43714907974f6bd31d6b9e2f04c6d793bf2b
Seung Ho Jang <shjang1992@hotmail.com>
2014-07-02 01:27:28 +0900

    Javascript move key presses made into function

--- nodejs_source/public/js/Controller/scene.js
+++ nodejs_source/public/js/Controller/scene.js
@@ -22,64 +22,46 @@ var GameScene = function () {
     var initControl = function () {
         var pm = game.pm;
 
+        var moveTo = function (x, y, z) {
+            if (isMoveKeyDown) {
+                return;
+            }
+
+            var dest = game.player.pos.clone();
+            dest.x += x;
+            dest.y += y;
+            dest.z += z;
+            game.player.moveTo(dest);
+
+            var pks = pm.createInstance("MOVE");
+            pks.x = x;
+            pks.y = y;
+            pks.z = z;
+            pm.send(pks);
+
+            isMoveKeyDown = true;
+        };
+
         jQuery(document).keydown(function (e) {
             switch (e.keyCode) {
                 case 65: // a
                     {
-                        if (isMoveKeyDown) {
-                            return;
-                        }
-
-                        var dest = game.player.pos.clone();
-                        dest.x -= moveDist;
-                        game.player.moveTo(dest);
-
-                        var pks = pm.createInstance("MOVE");
-                        pks.x = -moveDist;
-                        pks.y = 0;
-                        pks.z = 0;
-                        pm.send(pks);
-
-                        isMoveKeyDown = true;
+                        moveTo(-moveDist, 0, 0);
                     }
                     break;
                 case 83: // s
                     {
-                        if (isMoveKeyDown) {
-                            return;
-                        }
-
-                        var dest = game.player.pos.clone();
-                        dest.y -= moveDist;
-                        game.player.moveTo(dest);
-
-                        isMoveKeyDown = true;
+                        moveTo(0, -moveDist, 0);
                     }
                     break;
                 case 68: // d
                     {
-                        if (isMoveKeyDown) {
-                            return;
-                        }
-
-                        var dest = game.player.pos.clone();
-                        dest.x += moveDist;
-                        game.player.moveTo(dest);
-
-                        isMoveKeyDown = true;
+                        moveTo(moveDist, 0, 0);
                     }
                     break;
                 case 87: // w
                     {
-                        if (isMoveKeyDown) {
-                            return;
-                        }
-
-                        var dest = game.player.pos.clone();
-                        dest.y += moveDist;
-                        game.player.moveTo(dest);
-
-                        isMoveKeyDown = true;
+                        moveTo(0, moveDist, 0);
                     }
                     break;
             }
@@ -91,6 +73,9 @@ var GameScene = function () {
                 case 68: // d
                 case 87: // w
                     {
+                        var pks = pm.createInstance("STOP");
+                        pm.send(pks);
+
                         isMoveKeyDown = false;
                     }
                     break;
