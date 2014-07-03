b13f0919b3dc984244f165756d03f5b9a5ac4128
Seung Ho Jang <shjang1992@hotmail.com>
2014-07-04 01:00:59 +0900

    ranged for loop support for FieldMapManager has been added

--- cpp_source/ServerLib/FieldMapManager.cpp
+++ cpp_source/ServerLib/FieldMapManager.cpp
@@ -1,2 +1,22 @@
 #include "stdafx.h"
 #include "FieldMapManager.h"
+
+FieldMapManager::iterator FieldMapManager::begin()
+{
+	return mapList.begin();
+}
+
+FieldMapManager::iterator FieldMapManager::end()
+{
+	return mapList.end();
+}
+
+FieldMapManager::const_iterator FieldMapManager::begin() const
+{
+	return mapList.begin();
+}
+
+FieldMapManager::const_iterator FieldMapManager::end() const
+{
+	return mapList.end();
+}
--- cpp_source/ServerLib/FieldMapManager.h
+++ cpp_source/ServerLib/FieldMapManager.h
@@ -3,6 +3,13 @@
 class FieldMapManager
 {
 public:
+	typedef std::unordered_map<int, SPtrMap>::iterator iterator;
+	typedef std::unordered_map<int, SPtrMap>::const_iterator const_iterator;
+public:
+	iterator begin();
+	iterator end();
+	const_iterator begin() const;
+	const_iterator end() const;
 private:
 	std::unordered_map<int, SPtrMap> mapList;
 };
