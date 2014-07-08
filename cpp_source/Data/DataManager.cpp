adda5c588a07c423042848c904eda9ba7db1da2c
Seung Ho Jang <shjang1992@hotmail.com>
2014-07-09 01:12:48 +0900

    Parse test works

--- cpp_source/Data/DataManager.cpp
+++ cpp_source/Data/DataManager.cpp
@@ -1,14 +1,40 @@
 #include "DataManager.h"
 
-#include <filesystem>
+#include <fstream>
+using namespace std;
+
+#include "json/json.h"
 
 bool DataManager::LoadFile(const std::string& filename)
 {
-	std::tr2::sys::path path = filename;
-	if (std::tr2::sys::exists(path) == false)
+	ifstream input;
+	string content;
+
+	input.open(filename);
+
+	if (input.is_open())
+	{
+		string temp;
+
+		while (input)
+		{
+			temp.clear();
+			getline(input, temp);
+			content += '\n';
+			content += temp;
+		}
+	}
+	
+	input.close();
+
+	if (content.length() == 0)
 	{
 		return false;
 	}
+
+	Json::Reader reader;
+	Json::Value root;
+	reader.parse(content, root);
 
 	return true;
 }
