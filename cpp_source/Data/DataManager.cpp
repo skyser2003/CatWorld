189d4f27c796bd35c8ee75e8e5e9e8ad0e90f6cc
Seung Ho Jang <shjang1992@hotmail.com>
2014-07-10 01:30:16 +0900

    DataManager::LoadAllFiles function added Does not use 'bool isRecursive' argument yet

--- cpp_source/Data/DataManager.cpp
+++ cpp_source/Data/DataManager.cpp
@@ -1,9 +1,20 @@
 #include "DataManager.h"
 
 #include <fstream>
+#include <filesystem>
 using namespace std;
+using namespace std::tr2;
 
 #include "json/json.h"
+
+DataManager::DataManager()
+{
+
+}
+DataManager::~DataManager()
+{
+
+}
 
 bool DataManager::LoadFile(const std::string& filename)
 {
@@ -34,7 +45,34 @@ bool DataManager::LoadFile(const std::string& filename)
 
 	Json::Reader reader;
 	Json::Value root;
-	reader.parse(content, root);
+	bool success = reader.parse(content, root);
+
+	return success;
+}
+
+bool DataManager::LoadAllFiles(const std::string& path, bool isRecursive)
+{
+	sys::path searchPath = path;
+
+	for (sys::recursive_directory_iterator it(searchPath),
+		end;
+		it != end;
+		++it)
+	{
+		auto filePath = it->path();
+		if (sys::is_directory(filePath))
+		{
+			continue;
+		}
+
+		string filename = filePath.string();
+		
+		bool success = LoadFile(filename);
+		if (success == false)
+		{
+			return false;
+		}
+	}
 
 	return true;
 }
--- cpp_source/Data/DataManager.h
+++ cpp_source/Data/DataManager.h
@@ -5,6 +5,10 @@
 class DataManager
 {
 public:
+	DataManager();
+	~DataManager();
+
 	bool LoadFile(const std::string& filename);
+	bool LoadAllFiles(const std::string& path, bool isRecursive);
 private:
 };
