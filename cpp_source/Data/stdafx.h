444641cc20c9dfea33cabe27e417d1d57b1d443e
Seung Ho Jang <shjang1992@hotmail.com>
2014-07-11 01:28:45 +0900

    typedef.h added to Data project

--- cpp_source/Data/stdafx.h
+++ cpp_source/Data/stdafx.h
@@ -3,6 +3,10 @@
 // STL
 #include <fstream>
 #include <filesystem>
+#include <unordered_map>
+#include <memory>
 
 // External libraries
 #include "json/json.h"
+
+#include "typedef.h"
---
+++ cpp_source/Data/typedef.h
@@ -0,0 +1,5 @@
+#pragma once
+
+class DataProperty;
+
+typedef std::shared_ptr<DataProperty> SPtrProp;
