3267597d120ce27decae42a784d522f63ab12a59
Seung Ho Jang <shjang1992@hotmail.com>
2014-07-12 11:10:55 +0900

    DataClass owns properties

--- cpp_source/Data/DataClass.cpp
+++ cpp_source/Data/DataClass.cpp
@@ -1,2 +1,44 @@
 #include "stdafx.h"
 #include "DataClass.h"
+
+#include "DataProperty.h"
+
+void DataClass::AddProperty(const std::string& key, const SPtrProp& prop)
+{
+	if (propList.find(key) == propList.end())
+	{
+		propList.emplace(key, prop);
+	}
+	else
+	{
+		// Error!
+	}
+}
+
+SPtrProp DataClass::GetProperty(const std::string& key) const
+{
+	auto it = propList.find(key);
+
+	if (it == propList.end())
+	{
+		return SPtrProp();
+	}
+	else
+	{
+		return it->second;
+	}
+}
+
+SPtrProp DataClass::GetProperty(const std::string&& key) const
+{
+	auto it = propList.find(key);
+
+	if (it == propList.end())
+	{
+		return SPtrProp();
+	}
+	else
+	{
+		return it->second;
+	}
+}
--- cpp_source/Data/DataClass.h
+++ cpp_source/Data/DataClass.h
@@ -1,7 +1,23 @@
 #pragma once
 
+#include "DataType.h"
+
+class DataProperty;
+
 class DataClass
 {
 public:
+	void SetID(int id) { this->id = id; }
+	void SetName(const std::string& name) { this->name = name; }
+
+	int ID() const { return id; }
+	const std::string& Name() const { return name; }
+
+	void AddProperty(const std::string& key, const SPtrProp& prop);
+	SPtrProp GetProperty(const std::string& key) const;
+	SPtrProp GetProperty(const std::string&& key) const;
 private:
+	int id;
+	std::string name;
+	std::unordered_map<std::string, SPtrProp> propList;
 };
